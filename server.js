6082d5ccf113cf42af6fbad08f6333ea0e65d3c9
pgm9-art <pgm9@pm.me>
2026-02-03 15:48:29 -0800

    Update server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const RSSParser = require('rss-parser');
 const path = require('path');
+const fetch = require('node-fetch');
 require('dotenv').config();
 
 const app = express();
